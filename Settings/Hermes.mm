5633f285538ebe3e2123c8320cf9597463fec674
uhBlur <aditk96@gmail.com>
2014-08-10 18:33:47 -0500

    Update Hermes.mm

--- Settings/Hermes.mm
+++ Settings/Hermes.mm
@@ -89,12 +89,9 @@ int width = [[UIScreen mainScreen] bounds].size.width;
 @implementation HermesListController
 - (id)specifiers {
 	if(_specifiers == nil) {
-		_specifiers = [self loadSpecifiersFromPlistName:@"Hermes" target:self];
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Hermes" target:self] retain];
 	}
 	return _specifiers;
-}
--(void)respring {
-	system("killall backboardd");
 }
 @end
 @implementation HermesCreditsListController
@@ -185,11 +182,11 @@ int randNum = 0;
 			if (randNum == 0) randNum++;
 			else if (randNum < 10 && randNum != 0) randNum++;
 			//return @"From Codyd51";
-			return @"Is this thing on?";
+			return @"Icon by @uhBlurry";
 		case 7:
 			if (randNum == 0) randNum++;
 			else if (randNum < 10 && randNum != 0) randNum++;
-			return @"We love you, /r/jailbreak!";
+			return @"I love you, /r/jailbreak!";
 		case 8:
 			if (randNum == 0) randNum++;
 			else if (randNum < 10 && randNum != 0) randNum++;
@@ -237,7 +234,6 @@ int randNum = 0;
 		[self addSubview:underLabel];
 
 	}
-
 	return self;
 }
  
@@ -343,6 +339,53 @@ UIColor* color = [UIColor colorWithRed:200/255.0f green:200/255.0f blue:200/255.
 
 @end
 
+@implementation HermesGiantMakerCell4
+-(id)initWithStyle:(long long)style reuseIdentifier:(NSString *)reuseIdentifier {
+	if((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])){
+		UIImage *bkIm = [UIImage imageWithContentsOfFile:@"/Library/Application Support/hermesAdit.png"];
+		CGSize imageSize = bkIm.size;
+		CGRect imageRect = CGRectMake(0, 0, imageSize.width, imageSize.height);
+		UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0.0);
+		CGContextRef ctx = UIGraphicsGetCurrentContext();
+		// Create the clipping path and add it
+		UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:imageRect];
+		[path addClip];
+		[bkIm drawInRect:imageRect];
+		//CGContextSetStrokeColorWithColor(ctx, [color CGColor]);
+		[path setLineWidth:30.0f];
+		[path stroke];
+		UIImage *roundedImage = UIGraphicsGetImageFromCurrentImageContext();
+		UIGraphicsEndImageContext();
+		bkIm = roundedImage;
+
+		_background = [[UIImageView alloc] initWithImage:bkIm];
+		_background.frame = CGRectMake(9, 18, 65, 65);
+		[self addSubview:_background];
+		
+		CGRect frame = [self frame];
+		
+		label = [[UILabel alloc] initWithFrame:CGRectMake(frame.origin.x + 84, frame.origin.y + 18, frame.size.width, frame.size.height)];
+		[label setText:@"Adit Kamath"];
+		[label setBackgroundColor:[UIColor clearColor]];
+		if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+			[label setFont:[UIFont fontWithName:@"Helvetica Light" size:30]];
+		else
+			[label setFont:[UIFont fontWithName:@"Helvetica Light" size:21]];
+
+		[self addSubview:label];
+		
+		label2 = [[UILabel alloc] initWithFrame:CGRectMake(frame.origin.x + 84, frame.origin.y + 42, frame.size.width, frame.size.height)];
+
+		[label2 setText:@"Icon Designer"];
+		[label2 setBackgroundColor:[UIColor clearColor]];
+		[label2 setFont:[UIFont fontWithName:@"Helvetica Light" size:15]];
+		[self addSubview:label2];
+	}
+	return self;
+}
+
+@end
+
 @implementation hermesOpenTwitterPhillipController
 - (id)specifiers {
 	NSString *user = @"phillipten";
@@ -372,6 +415,32 @@ UIColor* color = [UIColor colorWithRed:200/255.0f green:200/255.0f blue:200/255.
 @implementation hermesOpenTwitterJasonController
 - (id)specifiers {
 	NSString *user = @"jaysan1292";
+	if([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetbot:"]])
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"tweetbot:///user_profile/" stringByAppendingString:user]]];
+	
+	else if([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitterrific:"]])
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"twitterrific:///profile?screen_name=" stringByAppendingString:user]]];
+	
+	else if([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetings:"]])
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"tweetings:///user?screen_name=" stringByAppendingString:user]]];
+	
+	else if([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitter:"]])
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"twitter://user?screen_name=" stringByAppendingString:user]]];
+	
+	else
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"https://mobile.twitter.com/" stringByAppendingString:user]]];
+	return 0;
+}
+
+- (void)viewDidAppear:(BOOL)arg1 {
+	UINavigationController *navController = self.navigationController;
+	[navController popViewControllerAnimated:YES];
+}
+@end
+
+@implementation hermesOpenTwitterAditController
+- (id)specifiers {
+	NSString *user = @"uhBlurry";
 	if([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetbot:"]])
 		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"tweetbot:///user_profile/" stringByAppendingString:user]]];
 	
